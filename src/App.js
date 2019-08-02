7a0dcbe7860598d214dc410da7ec2c2358f5f401
Krishan Nattar <krishan.nattar@gmail.com>
2019-08-02 12:07:02 -0700

    touchdown scores fixed

--- src/App.js
+++ src/App.js
@@ -35,11 +35,11 @@ function App() {
       <section className="buttons">
         <div className="homeButtons">
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-          <button className="homeButtons__touchdown" onClick ={()=>raiseHomeScore(homeScore + 6)}>Home Touchdown</button>
+          <button className="homeButtons__touchdown" onClick ={()=>raiseHomeScore(homeScore + 7)}>Home Touchdown</button>
           <button className="homeButtons__fieldGoal" onClick ={()=>raiseHomeScore(homeScore + 3)}>Home Field Goal</button>
         </div>
         <div className="awayButtons">
-          <button className="awayButtons__touchdown" onClick ={()=>raiseAwayScore(awayScore + 6)}>Away Touchdown</button>
+          <button className="awayButtons__touchdown" onClick ={()=>raiseAwayScore(awayScore + 7)}>Away Touchdown</button>
           <button className="awayButtons__fieldGoal" onClick ={()=>raiseAwayScore(awayScore + 3)}>Away Field Goal</button>
         </div>
       </section>
